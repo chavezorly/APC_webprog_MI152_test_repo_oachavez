f8ea1ddb5f187725a1090bc849b4611f2eba14e9
Orly Chavez <chavez.orly23@gmail.com>
2016-10-26 10:25:11 +0800

    24th

--- exercise5/index.php
+++ exercise5/index.php
@@ -1,16 +1,39 @@
-<?php      
+<?php
 include_once 'dbconfig.php';
 
 // delete condition
 if(isset($_GET['delete_id']))
-{ETE FROM users WHERE user_id=".$_GET['delete_id'];
+{
+ $sql_query="DELETE FROM users WHERE user_id=".$_GET['delete_id'];
  mysql_query($sql_query);
  header("Location: $_SERVER[PHP_SELF]");
 }
 // delete condition
 ?>
 
-<!DOCTYPE html 
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>CRUD Operations With PHP and MySql - By Cleartuts</title>
+<link rel="stylesheet" href="style.css" type="text/css" />
+<script type="text/javascript">
+function edt_id(id)
+{
+ if(confirm('Sure to edit ?'))
+ {
+  window.location.href='edit_data.php?edit_id='+id;
+ }
+}
+function delete_id(id)
+{
+ if(confirm('Sure to Delete ?'))
+ {
+  window.location.href='index.php?delete_id='+id;
+ }
+}
+</script>
+</head>
 <body>
 <center>
 
