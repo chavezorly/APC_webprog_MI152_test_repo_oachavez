97a2f55738f1188b0ff1b611b74fc1ee81f7c3e1
Orly Chavez <chavez.orly23@gmail.com>
2016-11-14 20:34:11 +0800

    9th

--- exercise6/codeigniter/application/views/Users_add.php
+++ exercise6/codeigniter/application/views/Users_add.php
@@ -3,9 +3,7 @@
 <html lang = "en">
  
    <head> 
-      <meta charset = "utf-8"> 
-      <title>Students Example</title> 
-      <link rel = "stylesheet" type = "text/css" href = "<?php echo base_url(); ?>css/style.css"> 
+
    </head> 
    <body> 
    <center>
